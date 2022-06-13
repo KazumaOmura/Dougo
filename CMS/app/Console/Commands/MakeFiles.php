e01b5ddaa639ab2be6421a10e8ae1677b05df724
KazumaOmura <omurakazuma.info@gmail.com>
2022-06-13 16:44:53 +0900

    Model 自動作成

--- CMS/app/Console/Commands/MakeFiles.php
+++ CMS/app/Console/Commands/MakeFiles.php
@@ -35,13 +35,54 @@ class MakeFiles extends Command
             // カラム情報を取得
             $columns = $table->getColumns();
 
-            $columnNames = [];
+            
+            $column_list = NULL;
+            $column_id = NULL;
             foreach ($columns as $column) {
-                array_push($columnNames, $column->getName());
+                if($column->getName() != 'id') {
+                    $column_list = $column_list . "'".$column->getName()."',";
+                } else {
+                    $column_id = "'id'";
+                }
+                // array_push($columnNames, $column->getName());
             }
 
-            echo "RepositoryFile作成中\n";
+            // Model作成
+            $model_value = <<< EOF
+<?php
 
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+            
+class Users extends Model
+{
+    public \$timestamps = false;
+            
+    protected \$table = 'users';
+            
+    protected \$fillable = [
+       $column_list
+    ];
+            
+    // アプリケーション側でcreateなどできない値を記述
+    protected \$guarded = [
+        $column_id
+    ];
+}
+              
+EOF;
+
+            $fpath = './app/Models/';
+            $fname = $fpath . $tableName_ucFirst . ".php";
+            $fhandle = fopen($fname, "w"); //ファイルを書き込みモードで開く。
+            fwrite($fhandle, $model_value); //ファイルをバイナリモードで書き込む。第二引数に書き込みたい文字列
+            fclose($fhandle); //ファイルポインタを閉じる
+
+
+
+
+
+            // Repository作成  
             $repo_value = <<< EOF
 <?php
 
@@ -53,7 +94,7 @@ class ${tableName_ucFirst}Repository
 {
     public static function getAll(int \$id):object
     {
-        \$all = Users::where('id', \$id)->get();
+        \$all = ${tableName_ucFirst}::where('id', \$id)->get();
         return \$all;
     }
 }        
@@ -64,38 +105,6 @@ EOF;
             $fhandle = fopen($fname, "w"); //ファイルを書き込みモードで開く。
             fwrite($fhandle, $repo_value); //ファイルをバイナリモードで書き込む。第二引数に書き込みたい文字列
             fclose($fhandle); //ファイルポインタを閉じる
-
-
-            // echo SampleRepository::getId(1);
-            // exit;
         }
     }
-
-    //         echo "RepositoryFile作成中\n";
-
-    //         $name = "Hoge";
-
-    //         $repo_value = <<< EOF
-    // <?php
-
-    // namespace App\ORM\Generated\Repository;
-
-    // use App\Models\Users;
-
-    // class ${name}Repository
-    // {
-    //     public static function getAll(int \$id):object
-    //     {
-    //         \$all = Users::where('id', \$id)->get();
-    //         return \$all;
-    //     }
-    // }        
-    // EOF;
-
-    //         $fpath = './app/ORM/Generated/Repository/';
-    //         $fname = $fpath . $name . "Repository.php";
-    //         $fhandle = fopen($fname, "w"); //ファイルを書き込みモードで開く。
-    //         fwrite($fhandle, $repo_value); //ファイルをバイナリモードで書き込む。第二引数に書き込みたい文字列
-    //         fclose($fhandle); //ファイルポインタを閉じる
-    //     }
 }
